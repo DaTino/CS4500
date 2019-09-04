43e06c5ac973cfa199dc478cacac401f1093743d
Alberto Maiocco <apmaiocco@gmail.com>
2019-09-04 11:31:53 -0500

    added depth first search and game results, as well as formatted output [am]

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -32,7 +32,74 @@ for i in specs[2:]:
     temp = i.split(" ");
     kList.append((int(temp[0]), int(temp[1])));
 
-print(f"Circles: {n}, Arrows: {k}, kList[0]: {kList[0]}");
-
+#creating an adjacency matrix. we'll use depth first search to visit each node.
+graph = {};
+#parsing tuple list to make matrix.
+for i in range(n):
+    graph[i+1] = [];
 for i in kList:
-   print(i);
+    graph[i[0]].append(i[1]);
+
+#Since we have a directed diagraph, we can use depth first search to determine
+#which nodes we visit. We use iterave as opposed to recursive depth first search
+#because, as a diagraph, the recursive algorithm will not end.
+#This version of the algorithm based off of Koder Dojo
+#https://www.koderdojo.com/blog/depth-first-search-in-python-recursive-and-non-recursive-programming
+#modified by me to generate the number of times a node is visited on our graph.
+def depthFirstIterative(graph, start):
+    stack = [start];
+    path  = [];
+    #Create a list of zeros to hold visited node information
+    nVisited = [];
+    for i in range(n):
+        nVisited.append(int(0));
+
+    while stack:
+        vertex = stack.pop();
+        nVisited[vertex-1] += 1;
+        if vertex in path:
+            continue;
+        path.append(vertex);
+        for neighbor in graph[vertex]:
+            stack.append(neighbor);
+    return nVisited;
+
+gameVals = depthFirstIterative(graph, 1);
+# 1. The number of circles used in the game.
+# 2. The number of arrows used in the game.
+# 3. The total number of checks on all the circles combined.
+# 4. The average number of checks in a circle marked during the game.
+# 5. The maximum number of checks in any one circle.
+totalChecks = 0;
+for i in gameVals:
+    totalChecks += i;
+avgChecks = totalChecks/n;
+maxChecks = max(gameVals);
+
+#Setting the output data. We use append() purely for code readability.
+#This is all output formatting.
+outputData = [["1. Number of Circles:", f"{n}"]];
+outputData.append(["2. Number of Arrows:", f"{k}"]);
+outputData.append(["3. Total # of Checks:", f"{totalChecks}"]);
+outputData.append(["4. Average # of Checks:", f"{avgChecks}"]);
+outputData.append(["5. Maximum # of Checks:", f"{maxChecks}"]);
+col_width = max(len(word) for row in outputData for word in row) + 2;  # padding
+
+#More screen output formatting.
+print(f"Game Results:");
+print(f"*************************");
+for row in outputData:
+    print("".join(word.ljust(col_width) for word in row));
+print(f"*************************");
+
+#outputting to file
+outfile = open("InClass3outfile.txt", "w");
+outfile.write(f"Game Results\n");
+outfile.write(f"*************************\n");
+for row in outputData:
+    outfile.write("".join(word.ljust(col_width) for word in row));
+    outfile.write("\n");
+outfile.write(f"*************************\n");
+outfile.close();
+
+input("Press Enter to terminate.");
